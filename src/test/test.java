0614ebc0cf76eb78fce6cc3ae4f3e19abecf515c
james <anasala.merthy@gmail.com>
2022-04-26 19:09:59 +0200

    maj 1

--- src/test/test.java
+++ src/test/test.java
@@ -1,7 +1,10 @@
+package test;
+
 public class test {
 
     public static void main(String[] args) {
 
         System.out.println("test");
+        System.out.println("je suis la 2e mise a jour");
     }
 }
